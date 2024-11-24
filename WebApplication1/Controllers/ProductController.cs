26fa0f5c4009c0874cedb0cdb9fb1b87de0fba00
candegersen <candeger.sen@hotmail.com>
2024-11-24 08:34:24 +0300

    Add project files.

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models.Product; // Ensure this matches your actual namespace
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product> products = new List<Product>
+        {
+            new Product
+            {
+                Id = 1,
+                Name = "Apple",
+                Description = "Smartphone designed by Apple.",
+                Price = 1299
+            },
+
+            new Product
+            {
+                Id = 2,
+                Name = "Samsung",
+                Description = "Smartphone designed by Samsung.",
+                Price = 899
+            },
+
+
+            new Product
+            {
+                Id = 2,
+                Name = "Huawei",
+                Description = "Smartphone designed by Huawei.",
+                Price = 799
+            },
+
+
+
+            new Product
+            {
+                Id = 2,
+                Name = "Xiaomi",
+                Description = "Smartphone designed by Xiaomi.",
+                Price = 699
+            }
+
+
+        };
+
+        public IActionResult Index()
+        {
+            return View(products); 
+        }
+    }
+}
---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Models.Product;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public int Price { get; set; }
+}
